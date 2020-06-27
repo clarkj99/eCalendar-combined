af10b1086b652d83b58f22526aa700a1ac586fdc
clarkj99 <clark@stontech.net>
2020-06-26 22:51:28 -0400

    testing

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/*', function (req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.render('index', { title: 'Express' });
+  // res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 module.exports = router;
