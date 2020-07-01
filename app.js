35f61ac7d51a339e43d5fbdc4117623081e0e421
clarkj99 <clark@stontech.net>
2020-07-01 00:51:14 -0400

    add react build and set URL to heroku server

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 9000;
 app.use(cors())
 app.use(logger('dev'));
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
