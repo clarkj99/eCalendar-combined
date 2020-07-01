d539e29e7db893962db350d81d9d47617e960fb4
clarkj99 <clark@stontech.net>
2020-07-01 00:54:20 -0400

    remove build from gitignore

---
+++ build/precache-manifest.53a231066c5fe260047aed87b0223317.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6ceaddf9afeec95e5fa36b971d7422",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d67e33d1e498d2253a5",
+    "url": "/static/css/main.b35b8585.chunk.css"
+  },
+  {
+    "revision": "5694d920149a8c4499c8",
+    "url": "/static/js/2.e00ba448.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e00ba448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d67e33d1e498d2253a5",
+    "url": "/static/js/main.caa4e793.chunk.js"
+  },
+  {
+    "revision": "94ba6f4ab554a35f6c3a",
+    "url": "/static/js/runtime-main.517b3ece.js"
+  },
+  {
+    "revision": "08dc08982033fd0025c02be7da14981a",
+    "url": "/static/media/sean-o-KMn4VEeEPR8-unsplash.08dc0898.jpg"
+  }
+]);
